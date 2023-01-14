9a2ae63f4dc8efe3be27a8ed9e8eecbf35f71429
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-01-14 10:35:05 -0500

    Working retrieval of current users in DB

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
-from typing import Sequence, Union
+from typing import Union
 
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
-from app import deps
-from app.dao.user_dao import user_dao
-from app.models.user import User
-from app.routers import institutions_router
+from app.routers import institutions_router, users_router
 
 app = FastAPI()
 
 app.include_router(institutions_router.router)
+app.include_router(users_router.router)
 
 
 @app.get("/")
@@ -27,9 +24,3 @@ def get_application_health():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
-
-@app.get("/users", status_code=200, response_model=Sequence[User])
-def get_users(db: Session = Depends(deps.get_db)) -> dict:
-    users = user_dao.get_all(db=db, limit=10)
-    return users
--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from typing import Sequence
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app import deps
+from app.dao.user_dao import user_dao
+from app.models.user import User
 
 router = APIRouter(
     prefix="/users",
@@ -9,3 +16,9 @@ router = APIRouter(
 @router.get("/{user_id}")
 def get_user(user_id: int):
     return {"user_id": user_id}
+
+
+@router.get("/", status_code=200, response_model=Sequence[User])
+def get_users(db: Session = Depends(deps.get_db)) -> dict:
+    users = user_dao.get_all(db=db, limit=10)
+    return users
