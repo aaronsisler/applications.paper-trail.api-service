ab5606abf91479caebd9d5a8f72966ab7df0a798
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-01-30 18:58:42 -0500

    Adding in the user create for dao and post for user rotuer

--- app/dao/user_dao.py
+++ app/dao/user_dao.py
@@ -1,11 +1,14 @@
 from typing import List, Optional, TypeVar, Type
 
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from app.dtos.base import Base
 from app.dtos.user_dto import UserDto
 
 ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 
 
 class UserDao:
@@ -18,12 +21,18 @@ class UserDao:
         """
         self.model = model
 
-    def get(self, db: Session, user_id: int) -> Optional[UserDto]:
+    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def read(self, db: Session, user_id: int) -> Optional[UserDto]:
         return db.query(self.model).filter(self.model.user_id == user_id).first()
 
-    def get_all(
-            self, db: Session, *, skip: int = 0, limit: int = 100
-    ) -> List[UserDto]:
+    def read_all(self, db: Session, *, skip: int = 0, limit: int = 100) -> List[UserDto]:
         return db.query(self.model).offset(skip).limit(limit).all()
 
 
--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -11,10 +11,15 @@ router = APIRouter(prefix="/users")
 
 
 @router.get("/{user_id}", response_model=Union[User, None])
-def get_user(user_id: int, db: Session = Depends(deps.get_db)):
-    return user_dao.get(db, user_id)
+def get_user(user_id: int, db: Session = Depends(deps.get_db)) -> dict:
+    return user_dao.read(db, user_id)
 
 
 @router.get("", status_code=200, response_model=Sequence[User])
-def get_users(db: Session = Depends(deps.get_db)) -> dict:
-    return user_dao.get_all(db=db, limit=10)
+def get_users(db: Session = Depends(deps.get_db)) -> list:
+    return user_dao.read_all(db=db, limit=10)
+
+
+@router.post("", status_code=200, response_model=User)
+def post_users(user: User, db: Session = Depends(deps.get_db)) -> dict:
+    return user_dao.create(db=db, obj_in=user)
